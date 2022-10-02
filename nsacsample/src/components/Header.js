3935216aa57f87511a9b563a2db827055646e173
Alekhya253 <94287192+Alekhya253@users.noreply.github.com>
2022-10-02 11:49:55 +0530

    added gallery cards

--- nsacsample/src/components/Header.js
+++ nsacsample/src/components/Header.js
@@ -14,9 +14,15 @@ function Header(){
                 <Navbar.Collapse id="basic-navbar-nav">
                 
                     <Nav className="ms-auto">
+<<<<<<< Updated upstream
                     <a className="nav-link text-white me-2" href="#home">{('Home')}</a>
                     <a className="nav-link text-white me-2" href="#mission">{('mission')}</a>
                     <a className="nav-link text-white" href="#gallery">{('gallery')}</a>
+=======
+                    <a className="nav-link me-2" href="#home">{('Home')}</a>
+                    <a className="nav-link me-2" href="#mission">{('mission')}</a>
+                    <a className="nav-link" href="#gallery">{('gallery')}</a>
+>>>>>>> Stashed changes
                     
                     
                     </Nav>
--- nsacsample/src/components/Home.js
+++ nsacsample/src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Home.css"
+import pic1 from "../components/images/venus.jpg";
 function Home() {
     return (
         <div className="container-fluid text-center bg-dark text-light">
@@ -18,12 +20,36 @@ function Home() {
             <p className="lead mt-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam eveniet, perspiciatis veritatis obcaecati perferendis quos alias illum molestiae magnam libero sequi maiores voluptate minus dolor ea, repellendus porro. Laboriosam consectetur dolore facilis sit, nostrum mollitia quod culpa ab unde deserunt molestiae? Voluptas pariatur tempore blanditiis perspiciatis debitis odit officiis quis corrupti sequi. Exercitationem, repudiandae aliquid asperiores, eum eveniet quidem maiores veniam similique provident quisquam assumenda fuga necessitatibus est vel inventore atque soluta. Odio, nobis perspiciatis explicabo accusamus eaque earum corrupti similique autem aut optio ratione neque, repudiandae veniam. Excepturi quam, non unde reiciendis perferendis tempore temporibus mollitia doloribus, beatae veniam consequatur et, minus doloremque dolore vero nisi voluptatibus voluptatum? Officiis sit aperiam nulla recusandae hic illum distinctio aliquid necessitatibus nobis molestiae. Quisquam, libero modi dolorum ad repellat accusantium earum vitae totam ea, reiciendis nam explicabo eligendi maxime quis odit. Nam at repudiandae laudantium deleniti optio! Velit, quaerat. Consectetur at non dolore beatae modi laudantium, neque, tempore quisquam quaerat quibusdam repudiandae nemo, ullam voluptatem sapiente pariatur consequuntur officia reiciendis. Nobis impedit incidunt est? Est impedit doloremque molestiae error, iste reiciendis ab nostrum consequuntur, eaque laudantium corporis quae, recusandae aliquid consequatur dolor a fugiat voluptas. Eos odio quia neque iste repudiandae facere?</p>
             </div>
             </div>
-            <div id="gallery">
-            
-                <h1>gallery</h1>
-
+            <div id = "gallery">
+                <div className="container">
+                    <h1>GALLERY</h1>
+                    <div className="row">
+                        <div className="col-6">
+                        <div className = "gallery-section-card shadow text-center">
+            <img
+            src={pic1}
+            className = "gallery-section-image"
+          />
+      <p className = "gallery-section-card-text">
+      NASA’s Parker Solar Probe captured stunning views of Venus during its close flyby of the planet in July 2020.
+      </p>
+    </div>
+                        </div>
+                        <div className="col-6">
+                        <div className = "gallery-section-card shadow text-center">
+            <img
+            src={pic1}
+            className = "gallery-section-image"
+          />
+      <p className = "gallery-section-card-text">
+      NASA’s Parker Solar Probe captured stunning views of Venus during its close flyby of the planet in July 2020.
+      </p>
+    </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+            </div>
       );
 }
 
