c3b70d2fa4b04a0ede067040ea467a61b0c381d4
anuraag2002 <anuraag2002@gmail.com>
2022-10-02 19:16:46 +0530

    edited gallery section and a few fonts

--- nsacsample/src/components/Home.js
+++ nsacsample/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css"
 import pic1 from "../components/images/venus.jpg";
+import milkyway from "../components/images/milkyway.jpeg"
 import bgimg from "../components/images/backimg.png"
 function Home() {
     return (
@@ -16,7 +17,7 @@ function Home() {
             </div>
             <div id="mission" className="pt-5">
             <div className="container pt-5">
-            <h1>mission</h1>
+            <h1>MISSION</h1>
             <p className="lead mt-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam eveniet, perspiciatis veritatis obcaecati perferendis quos alias illum molestiae magnam libero sequi maiores voluptate minus dolor ea, repellendus porro. Laboriosam consectetur dolore facilis sit, nostrum mollitia quod culpa ab unde deserunt molestiae? Voluptas pariatur tempore blanditiis perspiciatis debitis odit officiis quis corrupti sequi. Exercitationem, repudiandae aliquid asperiores, eum eveniet quidem maiores veniam similique provident quisquam assumenda fuga necessitatibus est vel inventore atque soluta. Odio, nobis perspiciatis explicabo accusamus eaque earum corrupti similique autem aut optio ratione neque, repudiandae veniam. Excepturi quam, non unde reiciendis perferendis tempore temporibus mollitia doloribus, beatae veniam consequatur et, minus doloremque dolore vero nisi voluptatibus voluptatum? Officiis sit aperiam nulla recusandae hic illum distinctio aliquid necessitatibus nobis molestiae. Quisquam, libero modi dolorum ad repellat accusantium earum vitae totam ea, reiciendis nam explicabo eligendi maxime quis odit. Nam at repudiandae laudantium deleniti optio! Velit, quaerat. Consectetur at non dolore beatae modi laudantium, neque, tempore quisquam quaerat quibusdam repudiandae nemo, ullam voluptatem sapiente pariatur consequuntur officia reiciendis. Nobis impedit incidunt est? Est impedit doloremque molestiae error, iste reiciendis ab nostrum consequuntur, eaque laudantium corporis quae, recusandae aliquid consequatur dolor a fugiat voluptas. Eos odio quia neque iste repudiandae facere?</p>
             <p className="lead mt-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam eveniet, perspiciatis veritatis obcaecati perferendis quos alias illum molestiae magnam libero sequi maiores voluptate minus dolor ea, repellendus porro. Laboriosam consectetur dolore facilis sit, nostrum mollitia quod culpa ab unde deserunt molestiae? Voluptas pariatur tempore blanditiis perspiciatis debitis odit officiis quis corrupti sequi. Exercitationem, repudiandae aliquid asperiores, eum eveniet quidem maiores veniam similique provident quisquam assumenda fuga necessitatibus est vel inventore atque soluta. Odio, nobis perspiciatis explicabo accusamus eaque earum corrupti similique autem aut optio ratione neque, repudiandae veniam. Excepturi quam, non unde reiciendis perferendis tempore temporibus mollitia doloribus, beatae veniam consequatur et, minus doloremque dolore vero nisi voluptatibus voluptatum? Officiis sit aperiam nulla recusandae hic illum distinctio aliquid necessitatibus nobis molestiae. Quisquam, libero modi dolorum ad repellat accusantium earum vitae totam ea, reiciendis nam explicabo eligendi maxime quis odit. Nam at repudiandae laudantium deleniti optio! Velit, quaerat. Consectetur at non dolore beatae modi laudantium, neque, tempore quisquam quaerat quibusdam repudiandae nemo, ullam voluptatem sapiente pariatur consequuntur officia reiciendis. Nobis impedit incidunt est? Est impedit doloremque molestiae error, iste reiciendis ab nostrum consequuntur, eaque laudantium corporis quae, recusandae aliquid consequatur dolor a fugiat voluptas. Eos odio quia neque iste repudiandae facere?</p>
             </div>
@@ -25,7 +26,8 @@ function Home() {
                 <div className="container">
                     <h1>GALLERY</h1>
                     <div className="row">
-                        <div className="col-12 col-md-6" >
+
+                        <div className="col-12 col-md-4" >
                         <div className = "gallery-section-card shadow text-center bg-secondary">
             <img
             src={pic1}
@@ -36,7 +38,21 @@ function Home() {
       </p>
     </div>
                         </div>
-                        <div className="col-12 col-md-6"  bg = "secondary">
+
+
+                        <div className="col-12 col-md-4"  bg = "secondary">
+                        <div className = "gallery-section-card shadow text-center bg-secondary">
+            <img
+            src={milkyway}
+            className = "gallery-section-image"
+          />
+      <p className = "gallery-section-card-text">
+      The image shows the first-light data from Parker Solar Probe's WISPR instrument suite. It shows Milky Way, looking at the galactic center.
+      </p>
+    </div>
+                        </div>
+
+                        <div className="col-12 col-md-4"  bg = "secondary">
                         <div className = "gallery-section-card shadow text-center bg-secondary">
             <img
             src={pic1}
