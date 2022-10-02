a590f8aa550739049fc0e7184e55d9bf2cfd7a8a
Alekhya253 <94287192+Alekhya253@users.noreply.github.com>
2022-10-02 22:07:15 +0530

    added 3d model

--- nsacsample/src/components/3dmodel.js
+++ nsacsample/src/components/3dmodel.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import "./model.css";
+import pspgif from "./images/psp1.gif"
 function Model(){
     return(
-        <div>
+        <div className="p-5 container-model">
+            <center>
+            <h1 className="model-heading p-5">3D Model of the Parker Solar Probe</h1>
+           
+            <iframe src='https://solarsystem.nasa.gov/gltf_embed/2356' width='40%' height='300px' frameborder='0' />
             
+            <br/>
+                <br/>
+            <h1 className="model-heading p-5">Orbit of the Parker Solar Probe</h1>
+            
+            <img src = {pspgif} width='40%' height='300px' />
+            </center>
         </div>
     )
 }
