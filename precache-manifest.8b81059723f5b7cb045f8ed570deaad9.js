fab7bb78969d93b1c838b26370298e9451e05986
anuraag2002 <anuraag2002@gmail.com>
2022-10-07 13:24:30 +0530

    Updates

---
+++ precache-manifest.8b81059723f5b7cb045f8ed570deaad9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5202f2f9e97e4d63b087",
+    "url": "/the-extraterrestrials/static/css/main.590ae6de.chunk.css"
+  },
+  {
+    "revision": "5202f2f9e97e4d63b087",
+    "url": "/the-extraterrestrials/static/js/main.5202f2f9.chunk.js"
+  },
+  {
+    "revision": "1fd78bf72b6aa9ef6d12",
+    "url": "/the-extraterrestrials/static/js/1.1fd78bf7.chunk.js"
+  },
+  {
+    "revision": "f1d21c48009812052c51",
+    "url": "/the-extraterrestrials/static/css/2.76fb7a43.chunk.css"
+  },
+  {
+    "revision": "f1d21c48009812052c51",
+    "url": "/the-extraterrestrials/static/js/2.f1d21c48.chunk.js"
+  },
+  {
+    "revision": "f83dfdec8d5b1ad5e11d",
+    "url": "/the-extraterrestrials/static/js/runtime~main.f83dfdec.js"
+  },
+  {
+    "revision": "78becf870ec37a6e9511c810c28dc615",
+    "url": "/the-extraterrestrials/static/media/venus.78becf87.jpg"
+  },
+  {
+    "revision": "0a6a7a2da95ee6d7431ae29693373a98",
+    "url": "/the-extraterrestrials/static/media/milkyway.0a6a7a2d.jpeg"
+  },
+  {
+    "revision": "12425e5a656df02264114a23b6c3fc47",
+    "url": "/the-extraterrestrials/static/media/psp.12425e5a.jpeg"
+  },
+  {
+    "revision": "30179ac94113bfb36778282ffcaa2d2f",
+    "url": "/the-extraterrestrials/static/media/whistler.30179ac9.ogg"
+  },
+  {
+    "revision": "8fc42e08d2c1d9fa7bc7b08293c4a194",
+    "url": "/the-extraterrestrials/static/media/dispersive.8fc42e08.ogg"
+  },
+  {
+    "revision": "1863a00493a1a7e530aa537bcdc5febb",
+    "url": "/the-extraterrestrials/static/media/psp1.1863a004.gif"
+  },
+  {
+    "revision": "d1edc28495ed60e9cc8be807155b88d9",
+    "url": "/the-extraterrestrials/static/media/backimg.d1edc284.png"
+  },
+  {
+    "revision": "6099842fdaa23fbcaa3260f73c528545",
+    "url": "/the-extraterrestrials/static/media/logo.6099842f.png"
+  },
+  {
+    "revision": "2d5c4ebf1f7de787d3a9b496c512765a",
+    "url": "/the-extraterrestrials/index.html"
+  }
+];
